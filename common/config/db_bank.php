4dbf499f164c28c955e637b861f1c04dbe48c23f
Jarmo Kortetjärvi <jarmo@futurable.fi>
2014-03-14 14:01:13 +0200

    Add default db config files

---
+++ common/config/db_bank.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+	'class' => 'yii\db\Connection',
+	'dsn' => 'mysql:host=futurality.fi;dbname=futural_bank',
+	'username' => 'futural_bank',
+	'password' => 'futural_core',
+	'charset' => 'utf8',
+];
