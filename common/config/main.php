c0b39a23b0e8f27bb32cb9c98e30cae8dccb3e1c
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-09-03 16:42:13 +0300

    Add missing attributes

--- common/config/main.php
+++ common/config/main.php
@@ -1,10 +1,16 @@
 <?php
+$db_core = require(__DIR__ . '/db_core.php');
+$db_bank = require(__DIR__ . '/db_bank.php');
+
 return [
-	'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
-	'extensions' => require(__DIR__ . '/../../vendor/yiisoft/extensions.php'),
-	'components' => [
-		'cache' => [
-			'class' => 'yii\caching\FileCache',
-		],
-	],
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'extensions' => require(__DIR__ . '/../../vendor/yiisoft/extensions.php'),
+    'components' => [
+        'db_core' => $db_core,
+        'db' => $db_bank,
+        'db_bank' => $db_bank,
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+    ],
 ];
